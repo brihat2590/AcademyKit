83d07f9f8a0e20451c9d89fcf45eefe06d28e095
brihat2590 <thapabrihatbir321@gmail.com>
2025-08-07 22:01:56 +0530

    added terms

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,86 @@
+export default function page(){
+    return(
+        <div className="bg-[#fbfef5] py-20 px-6 border-b-2 border-gray-300  ">
+            <div className="max-w-6xl mx-auto  text-gray-900 ">
+      <h1 className="text-4xl font-extrabold mb-6">AcademyKit Freemium Software Terms of Service</h1>
+
+      <p className="mb-6">
+      This Agreement ("Agreement") constitutes a binding contract between Sursa Technology ("Company") and any user ("User") who accesses or uses the AcademyKit software ("Software") by pulling the image from Docker Hub.
+
+
+      </p>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-extrabold mb-2">License and Usage</h2>
+        <p className="mb-2">
+        By pulling the AcademyKit image from Docker Hub, the User acknowledges and agrees to be bound by this Agreement.
+        </p>
+        <ul className="list-disc list-inside space-y-1">
+          <li>The User is granted a limited, non-exclusive, non-transferable right to use the Software for internal purposes only.</li>
+          <li>The usage of the Software is subject to the terms and limitations outlined in this Agreement.</li>
+          
+        </ul>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-extrabold mb-2">Usage Restrictions</h2>
+        
+        <ul className="list-disc list-inside space-y-1">
+          <li>The User shall not modify, reverse engineer, redistribute, or use the Software for any commercial purposes without explicit permission from the Company.</li>
+          <li>The Software may only be used within the scope of its intended functionality as provided by the Company.</li>
+          
+        </ul>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-extrabold mb-2">Payment and Charges</h2>
+        <p>
+        Usage of the Software beyond the limitations of the freemium version may result in charges. Charges are incurred based on the number of users or any other usage metrics specified by the Company.
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-extrabold mb-2">Ownership</h2>
+        <p>
+        The Company retains all rights, title, and interest in the Software, including any modifications or improvements made by the User.
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-extrabold mb-2">Disclaimer of Warranties</h2>
+        <p>
+        The Software is provided "as is" without warranties of any kind, expressed or implied. The Company does not guarantee the accuracy, reliability, or suitability of the Software for any particular purpose.
+
+
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-extrabold mb-2">Limitation of Liability</h2>
+        <p>
+        The Company shall not be liable for any damages arising from the use or inability to use the Software, including but not limited to indirect, incidental, or consequential damages.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-extrabold mb-2">Termination</h2>
+        <p>
+        The Company reserves the right to terminate or suspend access to the Software at any time without prior notice, especially in case of violation of this Agreement.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-extrabold mb-2">Governing Law</h2>
+        <p className="py-2">
+        This Agreement shall be governed by and construed in accordance with the laws of Nepal, without regard to its conflict of law provisions.
+
+
+        </p>
+        <p>By pulling the AcademyKit image from Docker Hub and using the Software, the User acknowledges that they have read, understood, and agreed to be bound by the terms and conditions of this Agreement. If the User does not agree with any part of this Agreement, they should refrain from using the Software.</p>
+      </section>
+      
+    </div>
+
+
+
+
+        </div>
+    )
+}
