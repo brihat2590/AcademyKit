453ea6586f02803da050ea8027a2b0364417a8d5
brihat2590 <thapabrihatbir321@gmail.com>
2025-08-07 22:22:21 +0530

    added links footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FaDiscord } from "react-icons/fa";
 
 export default function Footer() {
@@ -7,55 +8,55 @@ export default function Footer() {
         {/* Product */}
         <div>
           <h4 className="font-semibold text-gray-500 mb-4">Product</h4>
-          <ul className="space-y-3">
-            <li className="hover:text-[#85c226]">Features</li>
-            <li className="flex items-center gap-2 hover:text-[#85c226]">
+          <ul className="space-y-3 flex flex-col">
+            <Link href={"/why"} className="hover:text-[#85c226]">Features</Link>
+            <Link href={"/solutions"} className="flex items-center gap-2 hover:text-[#85c226]">
               Solutions
               <span className="text-xs bg-[#0b0b2b] text-white px-2 py-0.5 rounded-full">New</span>
-            </li>
-            <li className="hover:text-[#85c226]">Tutorials</li>
-            <li className="hover:text-[#85c226]">Pricing</li>
+            </Link>
+            <a href="https://docs.academykit.co/app-documentation/post-deployment/general" target="_blank" rel="noopener noreferrer" className="hover:text-[#85c226]">Tutorials</a>
+            <Link href={"/pricing"} className="hover:text-[#85c226]">Pricing</Link>
           </ul>
         </div>
 
         {/* Company */}
         <div>
           <h4 className="font-semibold text-gray-500 mb-4">Company</h4>
-          <ul className="space-y-3">
-            <li className="hover:text-[#85c226]">About us</li>
-            <li className="hover:text-[#85c226]">Media kit</li>
-            <li className="hover:text-[#85c226]">Contact</li>
+          <ul className="space-y-3 flex flex-col">
+            <Link href={"/about-us"} className="hover:text-[#85c226]">About us</Link>
+            <a href={"https://docs.academykit.co/app-documentation/quickstart"} target="_blank" rel="noopener noreferrer" className="hover:text-[#85c226]">Media kit</a>
+            <Link href={"/contact"} className="hover:text-[#85c226]">Contact</Link>
           </ul>
         </div>
 
         {/* Resources */}
         <div>
           <h4 className="font-semibold text-gray-500 mb-4 cursor-pointer">Resources</h4>
-          <ul className="space-y-3 ">
-            <li className="hover:text-[#85c226]">FAQs</li>
-            <li className="hover:text-[#85c226]">Request Demo</li>
-            <li className="hover:text-[#85c226]">Help center</li>
-            <li className="hover:text-[#85c226]">Support</li>
+          <ul className="space-y-3 flex flex-col ">
+            <Link href={"/FAQ"} className="hover:text-[#85c226]">FAQs</Link>
+            <Link href={"/request-demo"} className="hover:text-[#85c226]">Request Demo</Link>
+            <a href={"https://docs.academykit.co/app-documentation/introduction"} target="_blank" rel="noopener noreferrer" className="hover:text-[#85c226]">Help center</a>
+            <a href={"mailto:hello@academykit.co"} className="hover:text-[#85c226]">Support</a>
           </ul>
         </div>
 
         {/* Use Cases */}
         <div>
           <h4 className="font-semibold text-gray-500 mb-4">Use cases</h4>
-          <ul className="space-y-3">
-            <li className="hover:text-[#85c226]">Customer Success</li>
-            <li className="hover:text-[#85c226]">Employee Training</li>
-            <li className="hover:text-[#85c226]">Capacity Building</li>
-            <li className="hover:text-[#85c226]">On-Boarding</li>
+          <ul className="space-y-3 flex flex-col">
+            <Link href={"/customer-success"} className="hover:text-[#85c226]">Customer Success</Link>
+            <Link href={"/employee-training"} className="hover:text-[#85c226]">Employee Training</Link>
+            <Link href={"/capacity-building"} className="hover:text-[#85c226]">Capacity Building</Link>
+            <Link href={"/on-boarding"} className="hover:text-[#85c226]">On-Boarding</Link>
           </ul>
         </div>
 
         {/* Legal */}
         <div>
           <h4 className="font-semibold text-gray-500 mb-4">Legal</h4>
-          <ul className="space-y-3">
-            <li className="hover:text-[#85c226]">Terms</li>
-            <li className="hover:text-[#85c226]">Privacy</li>
+          <ul className="space-y-3 flex flex-col">
+            <Link href={"/terms"} className="hover:text-[#85c226]">Terms</Link>
+            <Link href={"/privacy"} className="hover:text-[#85c226]">Privacy</Link>
           </ul>
         </div>
 
@@ -63,8 +64,9 @@ export default function Footer() {
         <div>
           <h4 className="font-semibold text-gray-500 mb-4">Community</h4>
           <ul className="space-y-2">
-            <li className="flex items-center gap-1 hover:text-[#85c226]">
-              <FaDiscord /> Discord
+            <li className=" hover:text-[#85c226]">
+              <a href="https://discord.com/invite/cqeE5DKNuH" 
+              target="_blank" rel="noopener noreferrer" className="flex gap-2 items-center" ><FaDiscord /> Discord</a>
             </li>
           </ul>
         </div>
