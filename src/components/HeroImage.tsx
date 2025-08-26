3d17c7d469242343629febb84b69ec4f6158fe38
brihat2590 <thapabrihatbir321@gmail.com>
2025-08-26 14:49:12 +0530

    fix:hero image

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -7,11 +7,11 @@ const HeroImage: React.FC = () => {
   return (
     <section className="flex flex-col items-center justify-center py-8 bg-[#fbfef5]">
       <div className="max-w-4xl w-full px-4">
-        <Image
+        <img
           src={"https://s3.sursatech.com/api/v1/buckets/academykit/objects/download?preview=true&prefix=landing.jpg&version_id=null"}
           alt="Hero Screenshot"
           className="rounded-xl shadow-lg border border-gray-200"
-          layout="responsive"
+          
           width={1120}
           height={630}
         />
